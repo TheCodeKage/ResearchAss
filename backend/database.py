085db7b1ef5f848c554d4b70ff24a7474e9d7643
Naman Agrawal <nam.agr96@gmail.com>
2026-03-01 00:43:03 +0530

    rewrote requirements.txt

--- backend/database.py
+++ backend/database.py
@@ -7,6 +7,10 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL").replace("postgresql://", "postgresql+asyncpg://")
 
+if "sslmode=" in DATABASE_URL:
+        # Split by '?' to drop the query params or specifically replace 'sslmode=require'
+        DATABASE_URL = DATABASE_URL.split("?")[0]
+
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 AsyncSessionLocal = sessionmaker(
