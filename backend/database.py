6a37272cbb0fbca8ea144f58c49f469ff499254c
Naman Agrawal <nam.agr96@gmail.com>
2026-03-01 00:38:05 +0530

    rewrote requirements.txt

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql+asyncpg://myuser:mypassword@localhost/mydatabase"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
